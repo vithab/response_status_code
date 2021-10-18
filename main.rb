0bee8e27caa0ef925f55092fb9a3ca8878ea95f0
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-10-18 12:42:48 +0300

    Get code & status

--- main.rb
+++ main.rb
@@ -8,4 +8,13 @@ require 'write_xlsx'
 list_urls = File.readlines('fixtures/input_urls.txt').map(&:chomp)
 total_urls = list_urls.count
 
-p list_urls
+list_urls[0..2].each_with_index do |url, index|
+  puts "#{index + 1} from #{total_urls}\n\n#{url}"
+
+  uri = Addressable::URI.parse(url).normalize
+  response = Net::HTTP.get_response(uri)
+
+  p uri
+  p response.code
+  p response.message
+end
