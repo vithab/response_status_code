40a338cba18d1b76e424af04edf1a8f876d68400
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-10-19 12:00:46 +0300

    Add Nokogiri object, get meta data page

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require 'addressable'
+require 'addressable' # https://github.com/sporkmonger/addressable
 require 'json'
 require 'net/http'
 require 'nokogiri'
@@ -8,13 +8,21 @@ require 'write_xlsx'
 list_urls = File.readlines('fixtures/input_urls.txt').map(&:chomp)
 total_urls = list_urls.count
 
-list_urls[0..2].each_with_index do |url, index|
+list_urls[0..9].each_with_index do |url, index|
   puts "#{index + 1} from #{total_urls}\n\n#{url}"
 
   uri = Addressable::URI.parse(url).normalize
   response = Net::HTTP.get_response(uri)
+  
+  begin
+    uri_open = URI.open(uri)
+  rescue StandardError => e
+  end
 
-  p uri
+  doc = Nokogiri::HTML(uri_open)
+  
+  p doc.css('title').text
   p response.code
   p response.message
+  puts "="*80
 end
