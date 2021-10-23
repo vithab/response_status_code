7376ec1a6142b3ce6422d2c967d0e78592e0398d
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-10-24 00:16:42 +0300

    Refactoring exceptions & mapping "H1"

--- main.rb
+++ main.rb
@@ -4,34 +4,35 @@ require 'net/http'
 require 'nokogiri'
 require 'open-uri'
 require 'write_xlsx'
-require 'byebug'
+# require 'byebug'
 
 list_urls = File.readlines('fixtures/input_urls.txt').map(&:chomp)
 total_urls = list_urls.count
 
-list_urls[70..75].each_with_index do |url, index|
+list_urls[89..103].each_with_index do |url, index|
   puts "#{index + 1} from #{total_urls}\n\n#{url}"
   begin
     uri = Addressable::URI.parse(url).normalize
     response = Net::HTTP.get_response(uri)
     # byebug
-    begin
+    # begin
       uri_open = URI.open(uri)
-    rescue StandardError => e
-    end
+    # rescue StandardError => e
+    # end
 
     doc = Nokogiri::HTML(uri_open)
 
     p doc.css('title').text
-    p doc.css('h1').text
+    p doc.css('h1').map { |item| item.text.strip }.join('; ')
     p response.code
     p response.message
     puts "="*80
-  rescue SocketError => e
+  # rescue SocketError => e
+  rescue StandardError => e
     puts "Невозможно загрузить #{url}"
-    puts "Failed to open TCP connection to #{url}:80 (getaddrinfo: Name or service not known) (SocketError)"
+    puts e
     puts "="*80
-    
+
     next
   end
 end
