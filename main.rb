b8159a40176e4de549723400c9d8ef522252c675
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-10-23 23:10:36 +0300

    Add exception handler SocketError

--- main.rb
+++ main.rb
@@ -4,25 +4,34 @@ require 'net/http'
 require 'nokogiri'
 require 'open-uri'
 require 'write_xlsx'
+require 'byebug'
 
 list_urls = File.readlines('fixtures/input_urls.txt').map(&:chomp)
 total_urls = list_urls.count
 
-list_urls[0..9].each_with_index do |url, index|
+list_urls[70..75].each_with_index do |url, index|
   puts "#{index + 1} from #{total_urls}\n\n#{url}"
-
-  uri = Addressable::URI.parse(url).normalize
-  response = Net::HTTP.get_response(uri)
-  
   begin
-    uri_open = URI.open(uri)
-  rescue StandardError => e
-  end
+    uri = Addressable::URI.parse(url).normalize
+    response = Net::HTTP.get_response(uri)
+    # byebug
+    begin
+      uri_open = URI.open(uri)
+    rescue StandardError => e
+    end
 
-  doc = Nokogiri::HTML(uri_open)
-  
-  p doc.css('title').text
-  p response.code
-  p response.message
-  puts "="*80
+    doc = Nokogiri::HTML(uri_open)
+
+    p doc.css('title').text
+    p doc.css('h1').text
+    p response.code
+    p response.message
+    puts "="*80
+  rescue SocketError => e
+    puts "Невозможно загрузить #{url}"
+    puts "Failed to open TCP connection to #{url}:80 (getaddrinfo: Name or service not known) (SocketError)"
+    puts "="*80
+    
+    next
+  end
 end
