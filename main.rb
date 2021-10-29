ffcda6eb4c5b249e62d38cb590fdce50e9347201
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-10-29 21:07:47 +0300

    Check multiple title

--- main.rb
+++ main.rb
@@ -6,23 +6,25 @@ require 'open-uri'
 require 'write_xlsx'
 # require 'byebug'
 
-list_urls = File.readlines('fixtures/input_urls.txt').map(&:chomp)
+# list_urls = File.readlines('fixtures/input_urls.txt').map(&:chomp)
+list_urls = File.readlines('fixtures/test.txt').map(&:chomp)
 total_urls = list_urls.count
 
-list_urls[89..103].each.with_index(1) do |url, index|
+list_urls[4995..5000].each.with_index(1) do |url, index|
   puts "#{index} from #{total_urls}\n\n#{url}"
+
   begin
     uri = Addressable::URI.parse(url).normalize
     response = Net::HTTP.get_response(uri)
     uri_open = URI.open(uri)
 
     doc = Nokogiri::HTML(uri_open)
-
-    p doc.css('title').text
-    p doc.css('h1').map { |item| item.text.strip }.join('; ')
+    p doc.css('title').map { |item| item.text.chomp }.join('; ')
+    p doc.css('h1').map { |item| item.text.chomp }.join('; ')
     p response.code
     p response.message
     puts "="*80
+
   rescue StandardError => e
     puts "Невозможно загрузить #{url}"
     puts e
