f118b81136106e89ee8690bd6d27e1825f036a61
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-11-06 21:04:07 +0300

    collect data to hash structure

--- main.rb
+++ main.rb
@@ -11,50 +11,45 @@ require 'byebug'
 list_urls = File.readlines('fixtures/test.txt').map(&:chomp)
 total_urls = list_urls.count
 
-list_urls[4995..5000].each.with_index(1) do |url, index|
-  puts "#{index} from #{total_urls}\n\n#{url}"
+list_urls[4717..5000].each.with_index(1) do |url, index|
+  puts "#{index} from #{total_urls}\n\n"
 
   begin
     response_object = Response.new(url)
     uri = response_object.get_url_normalize
     response = response_object.get_response
 
-    uri_open = URI.open(uri)
-
-    page = PageParser.new(uri_open)
-    p page.get_title
-    p page.get_h1
-    p page.get_phone
-    p page.get_phone.class
-    # doc = Nokogiri::HTML(uri_open)
-    # p doc.css('title').map { |item| item.text.chomp }.join('; ')
-    # p doc.css('h1').map { |item| item.text.chomp }.join('; ')
-    
-    code = response.code
-    message = response.message
-
     case response
     when Net::HTTPInformation, Net::HTTPSuccess, Net::HTTPRedirection
       availability = 'Yes'
     when Net::HTTPClientError, Net::HTTPServerError
       availability = 'No'
-    else
-      code = 'No server response'
-      message = 'That address is incorrect'
-      availability = 'No'
     end
-    
-    p code
-    p message
-    p availability
+
+    uri_open = URI.open(uri)
+    page = PageParser.new(uri_open)
+
+    page = {
+      url:          url,
+      code:         response.code,
+      message:      response.message,
+      availability: availability,
+      phone:        page.get_phone,
+      email:        page.get_email,
+      title:        page.get_title,
+      h1:           page.get_h1,
+    }
+
+    p page
 
     puts "="*80
 
+  # TODO: сделать логер для ошибок; date, url, exception title
   rescue StandardError => e
     puts "Невозможно загрузить #{url}"
     puts e
     puts "="*80
 
-    next
+    # next
   end
 end
