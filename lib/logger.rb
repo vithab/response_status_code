1ee053b31aa02c10417b8cbd8f935e698a827108
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-11-24 17:08:47 +0300

    fix output string format

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,6 +1,6 @@
 class Logger
   def initialize
-    @prefix = "#{ --> Time.now.strftime('%d-%m-%Y_%H:%M:%S')}"
+    @prefix = " --> #{Time.now.strftime('%d-%m-%Y_%H:%M:%S')}"
   end
 
   def log(url, message)
