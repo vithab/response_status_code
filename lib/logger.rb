b0a6f3d6d5ae94677921607a4641e6168b6eebb3
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-11-24 17:23:35 +0300

    Add write to file method

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,13 +1,23 @@
 class Logger
+  SEPARATOR = ' --> '
+  
   def initialize
-    @prefix = " --> #{Time.now.strftime('%d-%m-%Y_%H:%M:%S')}"
+    @prefix = "#{Time.now.strftime('%d-%m-%Y_%H:%M:%S')}"
+    SEPARATOR 
   end
 
   def log(url, message)
-    $stdout.write "#{url} --> #{message}} #{@prefix}"
+    $stdout.write "#{url}#{SEPARATOR}#{message}#{SEPARATOR}#{@prefix}"
   end
 
   def write(url, message)
-    
+    File.open('logs/log.txt', 'a+:UTF-8') do |f|
+      f.write url
+      f.write SEPARATOR
+      f.write message
+      f.write SEPARATOR
+      f.write @prefix
+      f.write "\n"
+    end
   end
 end
