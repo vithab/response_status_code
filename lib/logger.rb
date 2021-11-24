45754aef61adffddfe525ac00be6b4eb479bd5c2
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-11-24 17:06:09 +0300

    Add prefix time

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,5 +1,13 @@
 class Logger
+  def initialize
+    @prefix = "#{ --> Time.now.strftime('%d-%m-%Y_%H:%M:%S')}"
+  end
+
   def log(url, message)
-    $stdout.write "#{url} --> #{message} --> #{Time.now.strftime('%d-%m-%Y_%H:%M:%S')}"
+    $stdout.write "#{url} --> #{message}} #{@prefix}"
+  end
+
+  def write(url, message)
+    
   end
 end
